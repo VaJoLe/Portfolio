9dd8647bbee437a81c84fa50a1daced766daead8
Jonas Valtin <138028521+VaJoLe@users.noreply.github.com>
2025-04-07 13:06:15 +0200

    addStructure&Font/completeHeader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ContactComponent } from "./contact/contact.component";
+import { HeroComponent } from './hero/hero.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferencesComponent } from './references/references.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillsComponent, ContactComponent, HeroComponent, PortfolioComponent, ReferencesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,8 +5,42 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  activeLanguage: string = 'DE'; // Standardwert
 
+
+
+
+
+
+
+  ngOnInit(): void {
+    const links = document.querySelectorAll<HTMLAnchorElement>('.menu a');
+
+    const activateLinkByHash = () => {
+      links.forEach((link) => link.classList.remove('active'));
+      const hash = window.location.hash;
+      const target = document.querySelector(`.menu a[href="${hash}"]`);
+      if (target) {
+        target.classList.add('active');
+      }
+    };
+
+    // Direkt beim Init aktivieren (falls schon Hash da ist)
+    activateLinkByHash();
+
+    // Click-Handler für alle Menü-Links
+    links.forEach((link) => {
+      link.addEventListener('click', () => {
+        setTimeout(activateLinkByHash, 0);
+      });
+    });
+  }
+
+
+  setLanguage(lang: string) {
+    this.activeLanguage = lang;
+  }
 }
