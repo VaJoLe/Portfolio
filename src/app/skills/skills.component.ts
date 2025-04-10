26d68f587fdfa1cd27e8ce87380791ac01e9d2af
Jonas Valtin <138028521+VaJoLe@users.noreply.github.com>
2025-04-10 18:56:38 +0200

    editSkills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  imageList: { title: string; image: string }[] = [
+    {
+      title: 'HTML',
+      image: '/assets/skills/html.png'
+    },
+    {
+      title: 'CSS',
+      image: '/assets/skills/CSS.png'
+    },
+    {
+      title: 'JavaScript',
+      image: '/assets/skills/Js.png'
+    },
+    {
+      title: 'TypeScript',
+      image: '/assets/skills/Ts.png'
+    },
+    {
+      title: 'Angular',
+      image: '/assets/skills/Angular.png'
+    },
+    {
+      title: 'Firebase',
+      image: '/assets/skills/Firebase.png'
+    },
+    {
+      title: 'Git',
+      image: '/assets/skills/Git.png'
+    },
+    {
+      title: 'Rest API',
+      image: '/assets/skills/API.png'
+    },
+    {
+      title: 'Scrum',
+      image: '/assets/skills/Scrum.png'
+    },
+    {
+      title: 'Material Design',
+      image: '/assets/skills/MD.png'
+    },
+    {
+      title: 'Continually Learning',
+      image: '/assets/skills/Interest.png'
+    }
+ ];
 
-}
+  }
