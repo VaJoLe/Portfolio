6f399ce9eda8c7ee1ec4f65c3070658c8acfda11
Jonas Valtin <138028521+VaJoLe@users.noreply.github.com>
2025-04-11 22:43:59 +0200

    firstEditsPortfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectComponent],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
+  projects = [
+    {
+      title: 'Join',
+      techStack: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      liveUrl: 'https://your-join-live-url.com',
+      githubUrl: 'https://github.com/your-repo/join',
+      imageUrl: 'assets/projects/Join.png',
+    },
+    {
+      title: 'ElPolloLoco',
+      techStack: 'JavaScript | HTML | CSS',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      liveUrl: 'https://your-sharkie-live-url.com',
+      githubUrl: 'https://github.com/your-repo/sharkie',
+      imageUrl: 'assets/projects/ElPolloLoco.png',
+    },
+    {
+      title: 'Pokedex',
+      techStack: 'JavaScript | HTML | CSS | API',
+      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      liveUrl: 'https://your-sharkie-live-url.com',
+      githubUrl: 'https://github.com/your-repo/sharkie',
+      imageUrl: 'assets/projects/Pokedex.png',
+    },
+    // weitere Projekte...
+  ];
 
 }
--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
 })
 export class ProjectComponent {
-
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() techStack!: string;
+  @Input() liveUrl!: string;
+  @Input() githubUrl!: string;
+  @Input() imageUrl!: string;
+  @Input() reversed: boolean = false;
 }
